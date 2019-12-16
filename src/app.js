0686e242c1d98b9c8662e898ba6f6e5cf825f9f1
Beatriz Gomez <beatrizgomez92@gmail.com>
2019-12-16 22:36:37 +0100

    WIP: getting data from back

--- src/app.js
+++ src/app.js
@@ -5,28 +5,73 @@ import requestAPI from './modules/services/service';
 
 const App = () => {
 
-    const [hotel, setHotel] = React.useState(10030559);
-    const [checkinDate, setCheckinDate] = React.useState('01/01/2020');
-    const [nights, setNigths] = React.useState(2);
+    const [hotel, setHotel] = React.useState(0);
+    const [checkinDate, setCheckinDate] = React.useState('');
+    const [nights, setNigths] = React.useState(0);
+    const [availableRates, setAvailableRates] = React.useState([]);
+    const [feedbackMessage, setFeedbackMessage] = React.useState('')
+    const [loading, setLoading] = React.useState(false)
 
 
     function getHotelFromApi() {
+        setLoading(true)
+
         requestAPI(hotel, checkinDate, nights)
             .then(response => {
-                console.log(response)
+                console.log(response.availableRates[hotel])
+                setAvailableRates(response.availableRates[hotel])
             })
+            .catch(console.error)
+            .finally(setLoading(false))
     }
 
-    function selectHotel(e) {
-        if (e.currentTarget.value === 44069509) {
-            setHotel(44069509)
+    function changeDateFormat(date) {
+        let splitDate = date.split('-');
+
+        if (splitDate.count === 0) {
+            return null;
         }
+
+        let year = splitDate[0];
+        let month = splitDate[1];
+        let day = splitDate[2];
+
+        return day + '/' + month + '/' + year;
+    }
+
+    function getCurrentDate() {
+        let date = new Date();
+
+        let day = date.getDate();
+        let month = date.getMonth();
+        let year = date.getFullYear();
+
+        return day + '/' + month + '/' + year;
+    }
+
+    function onSubmit(e) {
+
+        if (!!hotel && !!checkinDate && !!nights) {
+
+            if (checkinDate >= getCurrentDate) {
+                getHotelFromApi();
+            } else {
+                setFeedbackMessage('Introduzca una fecha valida. Ésta tiene que ser igual o superior al día actual. ☺️');
+            }
+
+            if (nights > 0 && nights < 30) {
+                getHotelFromApi();
+            } else {
+                setFeedbackMessage('Introduzca entre 1 y 30 noches. ☺️');
+            }
+
+        } else {
+            setFeedbackMessage('Complete todos los campos, por favor. ☺️');
+        }
+
         e.preventDefault();
     }
 
-
-    // recoger los datos de los inputs, y al dar a buscar que haga la petición,
-    // hacer un currentDate new Date
 
     return (
         <div className="app">
@@ -42,33 +87,69 @@ const App = () => {
             <main>
                 <section className={'form'}>
                     <h2>Select your hotel</h2>
-                    <div>
+                    <form>
                         <div>
-                            <label htmlFor={'baqueira-hotel'}>
-                                <span>Hotel Baqueira Val de Neu</span>
-                                <input id={'baqueira-hotel'} type="radio" value={44069509} name={'hotel'} onChange={selectHotel} />
-                            </label>
+                            <label></label>
+                            <select onChange={(e) => setHotel(e.currentTarget.value)}>
+                                <option defaultValue>Choose an option</option>
+                                <option value={44069509}>Hotel Baqueira Val de Neu</option>
+                                <option value={100376478}>Hotel Grand Luxor</option>
+                                <option value={10030559}>Hotel Moderno</option>
+                            </select>
                         </div>
                         <div>
-                            <label>
-                                <span>Hotel Grand Luxor</span>
-                                <input type="radio" value={100376478} name={'hotel'} onChange={selectHotel} />
-                            </label>
+                            <label></label>
+                            <input type="date" placeholder={'Check-in'} onChange={(e) => setCheckinDate(changeDateFormat(e.currentTarget.value))} />
                         </div>
                         <div>
-                            <label>
-                                <span>Hotel Moderno</span>
-                                <input type="radio" value={10030559} name={'hotel'} onChange={selectHotel} />
-                            </label>
+                            <label></label>
+                            <input type="number" placeholder={'Número de noches'} onChange={(e) => setNigths(e.currentTarget.value)} />
                         </div>
-                    </div>
-                    <div>
-                        <input type="date" placeholder={'Check-in'} />
-                    </div>
-                    <div>
-                        <input type="number" placeholder={'Número de noches'} />
-                    </div>
-                    <input type="submit" onClick={getHotelFromApi} />
+                        <input type={'submit'} onClick={onSubmit} value={'Buscar'} />
+                    </form>
+                </section>
+                <section className={'results'}>
+                    <h2>Resultados de su búsqueda</h2>
+                    {
+                        availableRates
+                            ? <div>
+                                <ul>
+                                    {availableRates.map((rates, i) =>
+                                        <li key={i}>
+                                            <ul>
+                                                <li>
+                                                    <h3>Nombre de la habitación</h3>
+                                                    {rates.roomName}
+                                                </li>
+                                                {rates.offerName ?
+                                                    <li>
+                                                        <h4>Nombre de la oferta</h4>
+                                                        {rates.offerName}
+                                                    </li>
+                                                    : ''}
+
+                                                <li>
+                                                    <h4>Régimen</h4>
+                                                    {rates.boardName}
+                                                </li>
+                                                <li>
+                                                    <h4>Ocupación</h4>
+                                                    {rates.occupancy.numAdults}
+                                                    {rates.occupancy.numChilds}
+                                                    {rates.occupancy.numBabies}
+                                                </li>
+                                                <li>
+                                                    <h4>Precio neto</h4>
+
+                                                    {}
+                                                </li>
+                                            </ul>
+                                        </li>)}
+                                </ul>
+                            </div>
+                            : 'Lo sentimos, no hay tarifas disponibles'
+                    }
+
                 </section>
             </main >
         </div >
---
+++ src/compontes/modal/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const Modal = () => {
+
+    return (
+        
+    )
+}
+
+export default Modal;
